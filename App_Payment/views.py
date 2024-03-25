0cd9f3af70f1a9b989c292220920c7f094753853
Rafid41 <wap.alnahiyan425@gmail.com>
2024-03-26 01:09:50 +0600

    payment

--- App_Payment/views.py
+++ App_Payment/views.py
@@ -8,13 +8,9 @@ from App_Payment.forms import BillingAddress, BillingForm, CollectCouponForm
 
 from django.contrib.auth.decorators import login_required
 
-import requests
-import socket
-from sslcommerz_python.payment import SSLCSession
-from decimal import Decimal
 
-# avoid csrf verification
-from django.views.decorators.csrf import csrf_exempt
+from decimal import Decimal
+import uuid
 
 # views
 
@@ -107,39 +103,6 @@ def payment(request):
         messages.info(request, f"Please complete profile detail")
         return redirect("App_Login:profile")
 
-    # https://pypi.org/project/sslcommerz-python/
-
-    # ---------------------------- info-1:  mypayment ------------------------#
-    #  sandbox na hoye real payment hole "sslc_is_sandbox=False", "sslc_store_id" & "sslc_store_pass" check from reply email from sslcoomerz,
-    mypayment = SSLCSession(
-        sslc_is_sandbox=True,
-        sslc_store_id="abcco656cf6297a654",
-        sslc_store_pass="abcco656cf6297a654@ssl",
-    )
-
-    # ---------------------------- info-2: mypayment.set_urls() ------------------------#
-
-    """
-    status_url = request.build_absolute_uri()
-    # eta current view er url k call kore
-    print(status_url)   #o/p: http://127.0.0.1:8000/payment/pay/
-    """
-    status_url = request.build_absolute_uri(reverse("App_Payment:complete"))
-
-    # print(status_url)   #http://127.0.0.1:8000/payment/status/
-
-    #'success_url' payment success hole kon page e jabe, fail/cancel hole kon page, 'ipn_url'= special kono notification thakle kon page
-    # we'll use a template here
-    mypayment.set_urls(
-        success_url=status_url,
-        fail_url=status_url,
-        cancel_url=status_url,
-        ipn_url=status_url,
-    )
-
-    # ---------------------------- info-3:  mypayment.set_product_integration() ------------------------#
-    # product info
-
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     order_items = order_qs[0].orderitems.all()
     order_items_count = order_qs[0].orderitems.count()
@@ -148,85 +111,24 @@ def payment(request):
     if coupon_percentise > 0:
         order_total = order_total - ((order_total * coupon_percentise) / 100)
 
-    mypayment.set_product_integration(
-        total_amount=Decimal(order_total),
-        currency="BDT",
-        product_category="Mixed",
-        product_name=order_items,
-        num_of_item=order_items_count,
-        shipping_method="Courier",
-        product_profile="None",
-    )
-
-    # ---------------------------- info-3:  mypayment.set_customer_info() ------------------------#
-    # kono ekta field blank thakle error dekhabe, ejnno upore egula chk kora hoise
-
-    current_user = request.user
-    # 'profile' == related_name
-    mypayment.set_customer_info(
-        name=current_user.profile.full_name,
-        email=current_user.email,
-        address1=current_user.profile.address_1,
-        address2=current_user.profile.address_1,
-        city=current_user.profile.city,
-        postcode=current_user.profile.zipcode,
-        country=current_user.profile.country,
-        phone=current_user.profile.phone,
-    )
-
-    # ---------------------------- info-3:  mypayment.set_shipping_info() ------------------------#
-    # Billing info
-    mypayment.set_shipping_info(
-        shipping_to=current_user.profile.full_name,
-        address=saved_address.address,
-        city=saved_address.city,
-        postcode=saved_address.zipcode,
-        country=saved_address.country,
-    )
-
-    # If you want to post some additional values
-    # mypayment.set_additional_values(value_a='cusotmer@email.com', value_b='portalcustomerid', value_c='1234', value_d='uuid')
-
-    response_data = mypayment.init_payment()
-    # print(response_data)    #{'status': 'SUCCESS', 'sessionkey': '86A9ED514AFD61A49124ED056FBBD665', 'GatewayPageURL': 'https://sandbox.sslcommerz.com/EasyCheckOut/testcde86a9ed514afd61a49124ed056fbbd665'}
-
-    # see the print(response_data) output to understand dict key 'GatewayPageURL'
-    # redirected to sslcommerz payment system
-    return redirect(response_data["GatewayPageURL"])
+    return complete(request)
 
 
-# accessing complete.html template// payment status (success/failure etc)
-# payment er pore info ei page dekhabe
-# @csrf_exempt deyar karone bangking er porer page(after pressing "success"/"successwith risk"/"failed" in payment bank/mobile_bank page)  e "post" method e ahob banking info gulo django show korbe(debug panel e ,error hole)
-@csrf_exempt
 def complete(request):
 
-    if request.method == "post" or request.method == "POST":
-        payment_data = request.POST
-        status = payment_data["status"]
+    val_id = str(uuid.uuid4())
+    tran_id = str(uuid.uuid4())
 
-        if status == "VALID":
-            val_id = payment_data["val_id"]
-            tran_id = payment_data["tran_id"]
-            messages.success(
-                request,
-                f"Your Payment Completed Successfully! Page will be redirected!",
+    if True:
+
+        messages.success(request, f"Your Payment Completed Successfully!")
+
+        return HttpResponseRedirect(
+            reverse(
+                "App_Payment:purchase",
+                kwargs={"val_id": val_id, "tran_id": tran_id},
             )
-
-            # call purchase()
-            return HttpResponseRedirect(
-                reverse(
-                    "App_Payment:purchase",
-                    kwargs={"val_id": val_id, "tran_id": tran_id},
-                )
-            )
-
-        else:
-            messages.warning(
-                request,
-                f"Your Payment Failed! Please Try Again! Page will be redirected!",
-            )
-
+        )
     return render(request, "App_Payment/complete.html", context={})
 
 
