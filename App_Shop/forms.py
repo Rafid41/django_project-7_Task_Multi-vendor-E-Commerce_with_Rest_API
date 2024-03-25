1b0c9951996d6822760a49fce68f5a3f39a2e115
Rafid41 <wap.alnahiyan425@gmail.com>
2024-03-25 15:31:37 +0600

    App_Shop

---
+++ App_Shop/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from App_Shop.models import Product
+
+
+# modify model structure
+class AddProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = [
+            "name",
+            "category",
+            "mainimage",
+            "preview_text",
+            "detail_text",
+            "price",
+            "old_price",
+        ]
